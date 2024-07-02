cd8fe4375ad8218d1682c7aad36108feb4f787f0
narasimhavrm <narasimhavrm@gmail.com>
2024-07-02 22:36:17 +0530

    mainci.groovy

---
+++ vars/mainci.groovy
@@ -0,0 +1,39 @@
+def call () {
+    node('workstation') {
+
+        if (env.cibuild == "nodejs") {
+            stage('Build ') {
+                sh 'npm install'
+            }
+        }
+        if (env.cibuild == "java") {
+            stage('Build ') {
+                sh 'mvn package'
+            }
+        }
+        stage('Unit Test ') {
+            echo 'Unit Test'
+//             sh 'npm test'
+        }
+
+        stage('Code Analysis ') {
+            echo 'Sonar '
+//            sh 'sonar-scanner -Dsonar.host.url=http://52.90.137.237:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+        }
+
+        stage('Security scan ') {
+            echo 'Security scan'
+        }
+
+        stage('Publish Artifact ') {
+            when {
+                expression {
+                    env.TAG_NAME ==~ ".*"
+                }
+            }
+            echo 'Publish Artifact'
+            sh 'env'
+        }
+
+    }
+}
