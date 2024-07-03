1cb6b7fd759ab1e7c1a601208043eb6755f49a44
narasimhavrm <narasimhavrm@gmail.com>
2024-07-03 11:21:54 +0530

    code for pushing artifact to sonar nexus

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,14 +1,26 @@
 def call () {
     node('workstation') {
 
+        stage('Code checkout' ) {
+            sh 'find . | grep "^./" | xargs rm -rf '
+            if(env.TAG_NAME ==~ ".*"){
+                env.gitbrname = "refs/tags/{env.TAG_NAME}"
+            } else {
+                env.gitbrname = "${env.BRANCH_NAME}"
+            }
+            checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/narasimhavrm/env.{component}" ]], branches: [[name: gitbrname]], poll: false]
+        }
+
         if (env.cibuild == "nodejs") {
             stage('Build ') {
-                sh 'npm install'
+                sh 'echo npm install'
+//                sh 'npm install'
             }
         }
         if (env.cibuild == "java") {
             stage('Build ') {
-                sh 'mvn package'
+                 sh 'echo mvn package'
+//                sh 'mvn package'
             }
         }
         stage('Unit Test ') {
@@ -28,7 +40,14 @@ def call () {
 
             if(env.TAG_NAME ==~ ".*") {
                 stage('Publish Artifact ') {
-                    echo 'Publish Artifact'
+                    sh 'rm -rf Jenkinsfile'
+                    sh 'echo ${TAG_NAME} > VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
+
+                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://nexusipaddress:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
+
+//                    echo 'Publish Artifact'
 //                    sh 'env'
 
                 }
