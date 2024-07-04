4159f8932fc861de96750d9712be521b5ee9a679
narasimhavrm <narasimhavrm@gmail.com>
2024-07-04 10:14:47 +0530

    nexus private ip address update in mainci.groovy

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -51,7 +51,7 @@ def call () {
                     sh 'echo ${TAG_NAME} > VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
 
-                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://3.86.233.151:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.240:8081/repository/${component}/${component}-${TAG_NAME}.zip'
 
 
 //                    echo 'Publish Artifact'
